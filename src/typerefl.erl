c4b421ac188f3abd7576605d39fb9563433e3059
k32 <10274441+k32@users.noreply.github.com>
2021-06-03 11:49:03 +0200

    Fix conversion of strings to binaries
    Fixes: #22

--- src/typerefl.erl
+++ src/typerefl.erl
@@ -231,7 +231,9 @@ atom() ->
 %% @doc Reflection of `binary()' type
 -spec binary() -> type().
 binary() ->
-  ?prim(binary, is_binary).
+  ?prim(binary, is_binary,
+        #{ from_string => fun(Str) -> {ok, erlang:list_to_binary(Str)} end
+         }).
 
 %% @doc Reflection of `boolean()' type
 -spec boolean() -> type().
